4b88abf34aef8f2552e33fa660b9698b4bc21a3e
Sachin <sachinsihmar6935@gmail.com>
2025-11-11 11:54:52 +0530

    Create Hibernate.java

---
+++ Hibernate.java
@@ -0,0 +1,13 @@
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    // getters and setters
+}
